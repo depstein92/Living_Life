01acf540d0715c672d0b1fa93a9b0cf00104bc40
Dan <d.epstein92@gmail.com>
2018-01-16 14:00:42 -0500

    installing semantic-ui

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
-import "semantic-ui-less/semantic.less";
 
 import App from './components/App';
 import LoginForm from './components/LoginForm';
--- webpack.config.js
+++ webpack.config.js
@@ -9,32 +9,18 @@ module.exports = {
     path: '/',
     filename: 'bundle.js'
   },
-  resolve: {
-     alias: {
-        '../../theme.config$': path.join(__dirname, 'semantic-ui/theme.config')
-     }
-  },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
-      },
-      {
-     use: ExtractTextPlugin.extract({
-       use: ['css-loader', 'less-loader']
-     }),
-     test: /\.less$/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'client/index.html'
-    }),
-    new ExtractTextPlugin({
-     filename: '[name].[contenthash].css',
-   }),
+    })
   ]
 };
