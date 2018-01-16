a1e31075da461d9788942a229a3536d19d88caa5
Dan <d.epstein92@gmail.com>
2018-01-16 14:25:51 -0500

    working on Auth component

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Button, Form, Segment } from 'semantic-ui-react';
 
 class AuthForm extends Component {
   constructor(props) {
@@ -17,16 +17,18 @@ class AuthForm extends Component {
   render() {
     return (
       <div className="row">
-        <form onSubmit={this.onSubmit.bind(this)} className="col s6">
+        <Form onSubmit={this.onSubmit.bind(this)} className="col s6">
           <div className="input-field">
-            <input
+            <Form.Input
+              fluid id='form-subcomponent-shorthand-input-first-name'
               placeholder="Email"
               value={this.state.email}
               onChange={e => this.setState({ email: e.target.value })}
             />
           </div>
           <div className="input-field">
-            <input
+            <Form.Input
+              fluid id='form-subcomponent-shorthand-input-first-name'
               placeholder="Password"
               type="password"
               value={this.state.password}
@@ -38,10 +40,24 @@ class AuthForm extends Component {
           </div>
           <Button>
           Submit</Button>
-        </form>
+        </Form>
       </div>
     );
   }
 }
+
+
+const FormExampleInverted = () => (
+  <Segment inverted>
+    <Form inverted>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='First name' placeholder='First name' />
+        <Form.Input fluid label='Last name' placeholder='Last name' />
+      </Form.Group>
+      <Form.Checkbox label='I agree to the Terms and Conditions' />
+      <Button type='submit'>Submit</Button>
+    </Form>
+  </Segment>
+)
 
 export default AuthForm;
