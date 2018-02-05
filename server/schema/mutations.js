396a292c1f34b6be3ce7587e989f1acfd1b28176
Dan <d.epstein92@gmail.com>
2018-02-05 18:04:43 -0500

    Mutations added to mutations.js in Schema

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -7,6 +7,7 @@ const {
 const UserType = require('./types/user_type');
 const AuthService = require('../services/auth');
 const axios = require('axios');
+const WordsSingleRelationType = require('./types/wordSingleRelationType');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -37,6 +38,18 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parentValue, { email, password }, req) {
         return AuthService.login({ email, password, req });
+      }
+    },
+    getWordsWithSingleRel: {
+      type: WordsSingleRelationType,
+      args: {
+            word: { type: GraphQLString },
+            typeOfQuery: { type: GraphQLString }
+      },
+      resolve(parentValue, { word, typeOfQuery }){
+        return axios.get(`https://api.datamuse.com/words?${ typeOfQuery }${ word }`)
+          .then(res => { return res.data })
+          .catch(err => console.log(err));
       }
     },
   }
